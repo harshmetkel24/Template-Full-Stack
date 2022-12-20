bf6a143e98c8e9977ce4387a906be68f63e0f394
unknown <harshmetkel24@gmail.com>
2022-12-21 01:41:37 +0530

    Basic template created

---
+++ backend/routes/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const { getHome } = require("../controllers/index");
+
+router.get("/", getHome);
+
+module.exports = router;
