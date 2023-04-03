6aa6ed76d386dd3a583ec38d3a9052744cbc6c94
Harshkumar Metkel <harshmetkel24@gmail.com>
2023-04-03 22:18:26 +0530

    removed secret files

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const { getHome, getIsha, getJourney } = require("../controllers/index");
+const { getHome } = require("../controllers/index");
 
 router.get("/", getHome);
-router.get("/isha", getIsha);
-router.get("/journey", getJourney);
 
 module.exports = router;
